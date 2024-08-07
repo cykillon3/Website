53d440a145a4d5e3a0209ad9593a07e597238a89
cykillon3 <110654808+cykillon3@users.noreply.github.com>
2024-08-07 17:17:28 +0500

    changed the three js directory to local files

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.128.0';
+import * as THREE from '/node_modules/three';
 
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/controls/OrbitControls.js';
-import { FBXLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/FBXLoader.js';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/GLTFLoader.js';
-import { VRMLLoader } from 'https://cdn.skypack.dev/three@0.128.0/examples/jsm/loaders/VRMLLoader.js';
+import { OrbitControls } from '/node_modules/three/examples/jsm/controls/OrbitControls.js';
+import { FBXLoader } from '/node_modules/three/examples/jsm/loaders/FBXLoader.js';
+import { GLTFLoader } from '/node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import { VRMLLoader } from '/node_modules/three/examples/jsm/loaders/VRMLLoader.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -18,10 +18,10 @@ camera.position.setZ(35);
 renderer.render(scene, camera);
 
 // Torus geometry
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({ color: 0x9999ff });
-const torus = new THREE.Mesh(geometry, material);
-scene.add(torus);
+// const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+// const material = new THREE.MeshStandardMaterial({ color: 0x9999ff });
+// const torus = new THREE.Mesh(geometry, material);
+// scene.add(torus);
 
 // Point Light
 const pointlight = new THREE.PointLight(0xffffff, 1);
@@ -49,28 +49,28 @@ Array(500).fill().forEach(addStar);
 scene.background = new THREE.Color(0x050505);
 
 // Moon
-const MoonTexture = new THREE.TextureLoader().load('assets/images/moon.jpg');
-const MoonNormalTexture = new THREE.TextureLoader().load('assets/images/normal.jpg');
-const moon = new THREE.Mesh(
-  new THREE.SphereGeometry(3, 32, 32),
-  new THREE.MeshStandardMaterial({
-    map: MoonTexture,
-    normalMap: MoonNormalTexture,
-  })
-);
-scene.add(moon);
-moon.position.z = 30;
-moon.position.setX(-10);
+// const MoonTexture = new THREE.TextureLoader().load('assets/images/moon.jpg');
+// const MoonNormalTexture = new THREE.TextureLoader().load('assets/images/normal.jpg');
+// const moon = new THREE.Mesh(
+//   new THREE.SphereGeometry(3, 32, 32),
+//   new THREE.MeshStandardMaterial({
+//     map: MoonTexture,
+//     normalMap: MoonNormalTexture,
+//   })
+// );
+// scene.add(moon);
+// moon.position.z = 30;
+// moon.position.setX(-10);
 
 function movecamera() {
   const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
+  // moon.rotation.x += 0.05;
+  // moon.rotation.y += 0.075;
+  // moon.rotation.z += 0.05;
 
-  camera.position.z = t * -0.01;
-  camera.position.x = t * -0.0002;
-  camera.position.y = t * -0.0002;
+  camera.position.z = t * 0.01;
+  camera.position.x = t * 0.0002;
+  camera.position.y = t * 0.0002;
 }
 
 document.body.onscroll = movecamera;
@@ -78,8 +78,8 @@ document.body.onscroll = movecamera;
 function animate() {
   requestAnimationFrame(animate);
 
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
+  // torus.rotation.x += 0.01;
+  // torus.rotation.y += 0.005;
 
   controls.update();
   renderer.render(scene, camera);
